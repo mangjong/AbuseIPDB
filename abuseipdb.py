2223bbe98ee0758931a17c12f4bfff6e56750815
mangjong <akdwhd0921@gmail.com>
2023-05-09 17:28:55 +0900

    Create abuseipdb.py

---
+++ abuseipdb.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import urllib3
+import sys
+import time
+import csv
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+start_time = time.time()
+
+url = 'https://api.abuseipdb.com/api/v2/check'
+
+headers = {
+        'Accept': 'application/json',
+        'Key': '4c90e8c703b5e330f210926a42626c3b7968208e7d1bc088d185d51db03d2cf6f858b24884072fef'
+}
+
+MAX_AGE_IN_DAYS = '60'
+
+def usage():
+    print("Usage: \n    python script.py [ IP Address | input_file ]")
+
+def write_csv(filename, rows):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['IP Address', 'Country Code', 'ISP', 'Domain', 'Report Count(60 Days)'])
+        writer.writerows(rows)
+
+def check_abuseIP(ip, rows):
+    params = {
+        'ipAddress': str(ip),
+        'maxAgeInDays': MAX_AGE_IN_DAYS,
+        'verbose': True
+    }
+    response = requests.get(url, headers=headers, params=params, verify=False)
+    decoded_response = response.json()
+    data = decoded_response.get('data', {})
+    ip_address = data.get('ipAddress', '')
+    country_code = data.get('countryCode', '')
+    isp = data.get('isp', '')
+    domain = data.get('domain', '')
+    reports = data.get('reports', [])
+    report_count = len(reports)
+
+    #result = f'{ip_address},{country_code},{isp},{domain}, {report_count}'
+    row = [ip_address, country_code, isp, domain, report_count]
+    rows.append(row)0
+
+    output = f'IP Address:    {ip_address}\n'
+    output += f'Country Code:  {country_code}\n'
+    output += f'ISP:           {isp}\n'
+    output += f'Domain:        {domain}\n'
+    output += f'Report Count:  {report_count}\n'
+    
+    print(json.dumps(decoded_response, sort_keys=True, indent=4))
+    
+    print(output)
+
+def main():
+    start_time = time.time()
+
+    CSV_FILENAME = 'result.csv'
+    rows = []
+
+    if len(sys.argv) == 2:
+        input_file = sys.argv[1]
+        
+        if not input_file.endswith('.txt'):
+            check_abuseIP(input_file, rows)
+        else:
+            with open(input_file, 'r') as f:
+                lines = f.readlines()
+            for line in lines:
+                ip = line.strip()
+                if ip:
+                    check_abuseIP(ip, rows)
+            write_csv(CSV_FILENAME, rows)
+        print(f'Total Time:    {round((time.time() - start_time), 2)} seconds')
+
+    else:
+        usage()
+    
+if __name__ == '__main__':
+    main()
